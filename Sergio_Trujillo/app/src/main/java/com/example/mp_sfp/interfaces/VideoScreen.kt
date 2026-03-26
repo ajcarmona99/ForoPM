9659d1ac3315857043288358d589e6cb5ac843e6
Sergio Trujillo <sergiotrujillonuez@gmail.com>
2026-03-26 17:30:54 +0100

    Añado .gitignore global y carpeta personal

---
+++ Sergio_Trujillo/app/src/main/java/com/example/mp_sfp/interfaces/VideoScreen.kt
@@ -0,0 +1,16 @@
+package com.example.mp_sfp.interfaces
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun VideoScreen() {
+    Column(Modifier.fillMaxSize().padding(24.dp, 48.dp, 0.dp, 0.dp)) {
+        Text("Video Screen")
+    }
+}
