2cf29ea30d8349e3fed3bb46c63440d2606fa6d0
Sergio Trujillo <sergiotrujillonuez@gmail.com>
2026-04-20 21:18:21 +0200

    Sesion 5 - Actualización de dependencias y limpieza de manifiesto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,19 +57,19 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation(platform(libs.androidx.compose.bom.v20241201))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.activity.compose.v193)
+    implementation(libs.androidx.navigation.compose)
 
     // CameraX
-    implementation("androidx.camera:camera-core:1.4.1")
-    implementation("androidx.camera:camera-camera2:1.4.1")
-    implementation("androidx.camera:camera-lifecycle:1.4.1")
-    implementation("androidx.camera:camera-view:1.4.1")
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
 
     // ExoPlayer (Media3)
-    implementation("androidx.media3:media3-exoplayer:1.5.1")
-    implementation("androidx.media3:media3-ui:1.5.1")
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
 }
--- app/src/main/java/com/example/mp_sfp/interfaces/VideoScreen.kt
+++ app/src/main/java/com/example/mp_sfp/interfaces/VideoScreen.kt
@@ -1,10 +1,9 @@
 package com.example.mp_sfp.interfaces
 
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.systemBarsPadding
 
 import androidx.compose.material3.Text
 
@@ -12,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 
 @Composable
